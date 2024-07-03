9d6b64853296edb2f29aa134c51b6f180e5c8dc8
Kristóf <122213466+Chris9402@users.noreply.github.com>
2024-07-03 12:37:50 +0200

    vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -2,16 +2,37 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "main"
+    Name = "main_vpc"
   }
 }
 
 resource "aws_subnet" "main" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "eu-central-1a"
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
 
   tags = {
-    Name = "main"
+    Name = "main_subnet"
+  }
+}
+
+resource "aws_security_group" "example" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "example_security_group"
   }
 }
