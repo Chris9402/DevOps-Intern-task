2d0bebe7b77659838b9e73e86c3d9605f6da84a3
Kristóf <122213466+Chris9402@users.noreply.github.com>
2024-07-03 12:35:47 +0200

    variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,17 @@
-variable "region" {
-  default = "eu-central-1"
+variable "aws_region" {
+  description = "The AWS region to deploy to"
+  type        = string
+  default     = "eu-central-1"
+}
+
+variable "instance_type" {
+  description = "The type of instance to create"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami" {
+  description = "The AMI ID to use for the instance"
+  type        = string
+  default     = "ami-01e444924a2233b07"
 }
